d54fe3af3320d54af3edccdaf9b0e85c3d9f26cb
Dien Bui <dienbuijoe@outlook.com>
2015-03-25 13:22:36 +0700

    demo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.compile = ['*.js', '*.css']
+  config.active_support.deprecation = :silence
 end

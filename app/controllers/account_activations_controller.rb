e7c9af936b607f83dae489adb6b1fe3fd701c2af
Dien Bui <dienbuijoe@outlook.com>
2015-05-12 21:43:56 +0700

    fix bugs

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,9 +4,7 @@ class AccountActivationsController < ApplicationController
 	    if user && !user.activated? && user.authenticated?(:activation, params[:id])
 	      user.activate
 	      log_in user
-	      flash[:success] = "Account activated!"	      
-	    else
-	      flash[:danger] = "Invalid activation link"	      
+	      remember(user)
 	    end
 
 	    redirect_to root_url
